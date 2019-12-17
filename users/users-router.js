47a44da234054856f2d1ba1586c75c41288f1b7d
Brad <brad@viziworx.com>
2019-12-17 16:43:40 -0500

    added restricted to users route

--- users/users-router.js
+++ users/users-router.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const Users = require('./users-model');
 
-router.get('/', (req, res) => {
+const restricted = require('../auth/restricted');
+
+router.get('/', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
