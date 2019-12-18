640c92e7b25437a9e7291d076b2f126cab0567cd
Brad <brad@viziworx.com>
2019-12-18 13:23:08 -0500

    updated file structure

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const authRouter = require('./auth/auth-router');
-const usersRouter = require('./users/users-router');
+const authRouter = require('../auth/auth-router');
+const usersRouter = require('../users/users-router');
 
 const server = express();
 
