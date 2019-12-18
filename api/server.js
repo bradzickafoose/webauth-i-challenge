13aecd67d30d8d662c8815edc2e2930b05fec889
Brad <brad@viziworx.com>
2019-12-18 13:43:24 -0500

    added connect-session-knex and session configuration

--- api/server.js
+++ api/server.js
@@ -4,13 +4,41 @@ const cors = require('cors');
 
 const authRouter = require('../auth/auth-router');
 const usersRouter = require('../users/users-router');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const server = express();
+
+const sessionConfiguration = {
+  // session storage options
+  name: "peanutbutter", // default would be sid
+  secret: "keep it secret, keep it safe!", // used for encryption (must be an environment variable)
+  saveUninitialized: true, // has implications with GDPR laws
+  resave: false,
+
+  // how to store the sessions
+  store: new KnexSessionStore({
+    // DO NOT FORGET THE new KEYWORD
+    knex, // imported from dbConfig.js
+    createtable: true,
+    clearInterval: 1000 * 60 * 10, // defaults to 6000
+    sidfieldname: 'sid', // Name it whatever you want. Defaults to 'sid'
+
+    // optional
+    tablename: 'sessions', // Name it whatever you want. Defaults to 'sessions'
+  }),
+
+  // cookie options
+  cookie: {
+    maxAge: 1000 * 60 * 10, // session will be good for 10 minutes in milliseconds
+    secure: false, // if false the cookie es sent over http, if true only sent over https
+    httpOnly: true, // if true JS cannot access the cookie
+  },
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
+server.use(sessions(sessionConfiguration)); // add a req.session object
 server.use('/api/', authRouter);
 server.use('/api/users', usersRouter);
 
